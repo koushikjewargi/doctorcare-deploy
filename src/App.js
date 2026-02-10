46817dff553e092f4cd4b6b1bdfd4eb3adcb424d
Aditya-shellagi <adityashellagi48@gmail.com>
2026-02-10 08:46:17 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Home from './pages/Home';
-import Booking from './pages/Booking';
-import Patient from './pages/Patient';
-import Payment from './pages/Payment';
-import Success from './pages/Success';
-import Failure from './pages/Failure';
-import History from './pages/History';
+import Sidebar from './components/Sidebar.js';
+import Home from './pages/Home.js';
+import Booking from './pages/Booking.js';
+import Patient from './pages/Patient.js';
+import Payment from './pages/Payment.js';
+import Success from './pages/Success.js';
+import Failure from './pages/Failure.js';
+import History from './pages/History.js';
 import './styles.css';
 
 function App() {
