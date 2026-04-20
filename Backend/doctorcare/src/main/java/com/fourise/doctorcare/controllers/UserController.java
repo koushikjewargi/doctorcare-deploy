157845c317e1d427b0492f708d48f515ca089bbf
koushik jewargi <jewargikoushik@gmail.com>
2026-04-21 00:22:17 +0530

    removed CROS conflits

--- Backend/doctorcare/src/main/java/com/fourise/doctorcare/controllers/UserController.java
+++ Backend/doctorcare/src/main/java/com/fourise/doctorcare/controllers/UserController.java
@@ -10,7 +10,6 @@ import com.fourise.doctorcare.services.UserService;
 import java.util.HashMap;
 import java.util.Map;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
